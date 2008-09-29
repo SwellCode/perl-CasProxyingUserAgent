3d4b53114f7db107c993d6a826731887333c2fb2
Daniel Frett <daniel.frett@ccci.org>
2008-09-29 20:47:12 +0000

    updated code to utilize callback handlers implemented in LWP 5.815

--- lib/Authen/CAS/LWP/ProxyingUserAgent.pm
+++ lib/Authen/CAS/LWP/ProxyingUserAgent.pm
@@ -4,238 +4,115 @@ use strict;
 use utf8;
 use base qw{LWP::UserAgent};
 
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
-use LWP 5.805;
+use HTTP::Status 5.811 ();
+use LWP 5.815;
 use URI;
+use URI::QueryParam;
 
+##Static Methods
+
+#Constructor
 sub new($%) {
-	my ($class, %settings) = @_;
-	my $self = $class->SUPER::new(%settings);
+	my $self = shift;
+	my (%settings) = @_;
+	$self = $self->SUPER::new(%settings);
 
 	#CAS attributes
 	$self->{'pgt'} = $settings{'pgt'};
 	$self->{'casServer'} = $settings{'casServer'};
 	$self->{'casServer'} =~ s/\/*$//og;
 
+	#add proxy request callback handler is casServer and PGT are set
+	if($self->{'pgt'} && $self->{'casServer'}) {
+		$self->set_my_handler('response_redirect', \&casProxyRequest,
+			'm_code' => [
+				HTTP::Status::RC_MOVED_PERMANENTLY,
+				HTTP::Status::RC_FOUND,
+				HTTP::Status::RC_SEE_OTHER,
+				HTTP::Status::RC_TEMPORARY_REDIRECT,
+			],
+		);
+	}
+
 	return $self;
 }
 
+##Instance Methods
 
+#method that tests to see if the specified uri is a cas login uri
+sub _isCASLoginURI($$) {
+	my $self = shift;
+	my ($targetURI) = @_;
 
-##copied from LWP::UserAgent.pm and modified to include CAS code
-##maybe move the CAS code to a more appropriate location in the LWP::UserAgent code in the future
-sub request
-{
-	my($self, $request, $arg, $size, $previous) = @_;
+	#make targetURI a URI object and clean off un-necessary params
+	$targetURI = URI->new($targetURI);
+	$targetURI->fragment(undef);
+	$targetURI->query(undef);
 
-	LWP::Debug::trace('()');
-
-	my $response = $self->simple_request($request, $arg, $size);
-
-	my $code = $response->code;
-	$response->previous($previous) if defined $previous;
-
-	LWP::Debug::debug('Simple response: ' .
-	(HTTP::Status::status_message($code) ||
-	"Unknown code $code"));
-
-	if ($code == &HTTP::Status::RC_MOVED_PERMANENTLY or
-	$code == &HTTP::Status::RC_FOUND or
-	$code == &HTTP::Status::RC_SEE_OTHER or
-	$code == &HTTP::Status::RC_TEMPORARY_REDIRECT)
-	{
-		my $referral = $request->clone;
-
-		# These headers should never be forwarded
-		$referral->remove_header('Host', 'Cookie');
-
-		if ($referral->header('Referer') &&
-		$request->url->scheme eq 'https' &&
-		$referral->url->scheme eq 'http')
-		{
-			# RFC 2616, section 15.1.3.
-			LWP::Debug::trace("https -> http redirect, suppressing Referer");
-			$referral->remove_header('Referer');
-		}
-
-		# And then we update the URL based on the Location:-header.
-		my $referral_uri = $response->header('Location');
-		{
-			# Some servers erroneously return a relative URL for redirects,
-			# so make it absolute if it not already is.
-			local $URI::ABS_ALLOW_RELATIVE_SCHEME = 1;
-			my $base = $response->base;
-			$referral_uri = "" unless defined $referral_uri;
-			$referral_uri = $HTTP::URI_CLASS->new($referral_uri, $base)
-			->abs($base);
-		}
-		$referral->url($referral_uri);
-
-		# Check for loop in the redirects, we only count
-		my $count = 0;
-		my $r = $response;
-		while ($r) {
-			if (++$count > $self->{max_redirect}) {
-				$response->header("Client-Warning" =>
-				"Redirect loop detected (max_redirect = $self->{max_redirect})");
-				return $response;
-			}
-			$r = $r->previous;
-		}
-
-
-
-
-		#####
-		#####CAS redirection code
-		#####
-		#check to see if CAS variables are defined before proceeding
-		if($self->{'pgt'} && $self->{'casServer'})
-		{
-			#initialize variables
-			my $new_url = URI->new_abs(scalar $response->header('Location'),$request->url)->canonical;
-			my $new_url2 = $new_url->clone;
-			$new_url2->fragment(undef);
-			$new_url2->query(undef);
-			my $cas_url = URI->new($self->{'casServer'} . '/login')->canonical;
-
-			#CAS redirection? if so, retrieve Proxy Ticket using PGT
-			if($cas_url->eq($new_url2))
-			{
-				#retrieve the service name
-				my @query_params = $new_url->query_form;
-				my $service = undef;
-				while(scalar @query_params && !defined $service)
-				{
-					my $key = shift @query_params;
-					my $value = shift @query_params;
-					if($key eq 'service')
-					{
-						$service = $value;
-					}
-				}
-
-				#retrieve a proxy ticket for the service
-				my $ticket = $self->getPT($service);
-
-				#if a ticket is retrieved successfully, reissue initial request or issue a new request depending on the service URL
-				if($ticket)
-				{
-					$new_url = URI->new($service . ($service =~ /\?/o ? '&' : '?') . 'ticket=' . $ticket);
-					$new_url2 = URI->new($service);
-
-					$referral->url($new_url);
-
-					#if service is equal to the original request, force re-request because we recieved a valid ticket from CAS
-					if($new_url2->eq($request->url))
-					{
-						return $self->request($referral, $arg, $size, $response);
-					}
-				}
-
-			}
-		}
-		#####
-		#####end of CAS redirection code
-		#####
-
-
-		#moved below CAS code so a CAS redirected POST can be re-issued if needed
-		if ($code == &HTTP::Status::RC_SEE_OTHER ||
-		$code == &HTTP::Status::RC_FOUND)
-		{
-			my $method = uc($referral->method);
-			unless ($method eq "GET" || $method eq "HEAD") {
-				$referral->method("GET");
-				$referral->content("");
-				$referral->remove_content_headers;
-			}
-		}
-
-		return $response unless $self->redirect_ok($referral, $response);
-		return $self->request($referral, $arg, $size, $response);
-	}
-	elsif ($code == &HTTP::Status::RC_UNAUTHORIZED ||
-	$code == &HTTP::Status::RC_PROXY_AUTHENTICATION_REQUIRED
-	)
-	{
-		my $proxy = ($code == &HTTP::Status::RC_PROXY_AUTHENTICATION_REQUIRED);
-		my $ch_header = $proxy ?  "Proxy-Authenticate" : "WWW-Authenticate";
-		my @challenge = $response->header($ch_header);
-		unless (@challenge) {
-			$response->header("Client-Warning" =>
-			"Missing Authenticate header");
-			return $response;
-		}
-
-		require HTTP::Headers::Util;
-		CHALLENGE: for my $challenge (@challenge) {
-			$challenge =~ tr/,/;/;  # "," is used to separate auth-params!!
-			($challenge) = HTTP::Headers::Util::split_header_words($challenge);
-			my $scheme = lc(shift(@$challenge));
-			shift(@$challenge); # no value
-			$challenge = { @$challenge };  # make rest into a hash
-			for (keys %$challenge) {       # make sure all keys are lower case
-				$challenge->{lc $_} = delete $challenge->{$_};
-			}
-
-			unless ($scheme =~ /^([a-z]+(?:-[a-z]+)*)$/) {
-				$response->header("Client-Warning" =>
-				"Bad authentication scheme '$scheme'");
-				return $response;
-			}
-			$scheme = $1;  # untainted now
-			my $class = "LWP::Authen::\u$scheme";
-			$class =~ s/-/_/g;
-
-			no strict 'refs';
-			unless (%{"$class\::"}) {
-				# try to load it
-				eval "require $class";
-				if ($@) {
-					if ($@ =~ /^Can\'t locate/) {
-						$response->header("Client-Warning" =>
-						"Unsupported authentication scheme '$scheme'");
-					}
-					else {
-						$response->header("Client-Warning" => $@);
-					}
-					next CHALLENGE;
-				}
-			}
-			unless ($class->can("authenticate")) {
-				$response->header("Client-Warning" =>
-				"Unsupported authentication scheme '$scheme'");
-				next CHALLENGE;
-			}
-			return $class->authenticate($self, $proxy, $challenge, $response,
-			$request, $arg, $size);
-		}
-		return $response;
-	}
-	return $response;
+	#test if URI is for the cas login page
+	return $targetURI->eq($self->{'casServer'} . '/login');
 }
 
-sub getPT($$)
-{
-	my ($self, $service) = @_;
+#method to get a PT for the specified service
+sub getPT($$) {
+	my $self = shift;
+	my ($service) = @_;
 
-	if(defined $service && $self->{'pgt'} && $self->{'casServer'})
-	{
+	if($service && $self->{'pgt'} && $self->{'casServer'}) {
 		my $PT_url = URI
 			->new($self->{'casServer'} . '/proxy')
 			->canonical;
 		$PT_url->query_form('targetService', $service, 'pgt', $self->{'pgt'});
 		my $response = $self->simple_request(HTTP::Request->new('GET' => $PT_url));
 		#if proxy ticket is retrieved successfully, return it
-		if($response->content =~ /<cas:proxyTicket>(.*?)<\/cas:proxyTicket>/o)
-		{
+		if($response->content =~ /<cas:proxyTicket>(.*?)<\/cas:proxyTicket>/o) {
 			return $1;
 		}
-		return '';
 	}
-	return undef;
+
+	return;
+}
+
+##Callback functions
+
+#callback that will handle retrieving a CAS PT and reissue the original request
+sub casProxyRequest($$$) {
+	my ($response, $ua, $h) = @_;
+
+	#check to see if CAS attributes are defined before proceeding
+	if($ua->{'pgt'} && $ua->{'casServer'}) {
+		my $request = $response->request;
+		my $targetURI = URI->new_abs(scalar $response->header('Location'), $request->uri)->canonical;
+
+		#CAS redirection?
+		if($ua->_isCASLoginURI($targetURI)) {
+			#find the requested service name
+			my $service = $targetURI->query_param('service');
+
+			#short-circuit if no service is found
+			return if(!$service);
+
+			#if a proxy ticket is retrieved successfully, reissue initial request with the proxy ticket
+			my $ticket = $ua->getPT($service);
+			if($ticket) {
+				my $newURI = URI->new($service . ($service =~ /\?/o ? '&' : '?') . 'ticket=' . $ticket);
+				#update the Location header in the response
+				$response->header('Location' => $newURI->as_string);
+
+				#if the service is the same as the original request uri, reissue the request to keep any request content
+				if($request->uri->eq($service)) {
+					my $newRequest = $request->clone;
+					$newRequest->uri($newURI);
+					return $newRequest;
+				}
+			}
+		}
+	}
+
+	#use default processing
+	return;
 }
 
 1;
